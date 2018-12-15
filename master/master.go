0f406f19f48fbfca272ffa8a0748b345a51edc69
Andrea <andrealeo09@gmail.com>
2018-12-15 18:13:31 +0100

    second commit

--- master/master.go
+++ master/master.go
@@ -10,9 +10,35 @@ import (
 // Work ciao
 type Work struct{}
 
+type infoSlave struct {
+	address string
+	port    string
+}
+
+var slaveConnected []infoSlave
+
 // WordCount fnaculoi
 func (t *Work) WordCount(msg *st.StringMsg, result *st.StringMsg) error {
 	result.Text = strings.ToUpper(msg.Text)
+	return nil
+}
+
+// JoinRequest message from slave to join master
+type JoinRequest struct {
+	address string
+	port    string
+}
+
+// ResponseRequest send from master to slave
+type ResponseRequest struct {
+	responseMessage string
+}
+
+// Join use by slave to contact master
+func (j *JoinRequest) Join(join *JoinRequest, result *ResponseRequest) error {
+	s := infoSlave{join.address, join.port}
+	slaveConnected = append(slaveConnected, s)
+	result.responseMessage = "Join done"
 	return nil
 }
 
@@ -22,6 +48,8 @@ func main() {
 
 	//server := rpc.NewServer()
 	server := com.RegisterRPCNamedService("Work", work)
+	join := new(JoinRequest)
+	server.RegisterName("Join", join)
 
 	l := com.CreatePortListener("localhost:1234")
 
--- slave/slave.go
+++ slave/slave.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"flag"
+	"fmt"
+	com "go-exercise/libs/comunication"
+	"log"
+)
+
+var serverAddress = flag.String("server", "localhost:1234", "master server")
+
+func main() {
+
+	server := com.ConnectToHost(*serverAddress)
+
+	//todo passare riferimento alle strutture di join del master
+
+	err := server.Call("Work.WordCount", msgToSend, &msgFromServer)
+	if err != nil {
+		log.Fatal("Error in Work.WordCount: ", err)
+	}
+	fmt.Printf("Work.WordCount: %s %s\n", msgToSend.Text, msgFromServer.Text)
+
+}
